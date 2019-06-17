829c0db4a4f1680d3720e80c01e0628d6f2f68cb
Anthony Ng <newyork.anthonyng@gmail.com>
2019-06-17 06:50:13 -0400

    Add accessibility for tabs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,75 @@
-console.log("%cHello World", "background-color: tomato; color: white; font-size: 24px;");
+// Start: Handle tabs
+const $tabs = document.querySelectorAll(`[role="tab"]`);
+const $panels = document.querySelectorAll(`[role="tabpanel"]`);
+
+const KEYS = {
+    end: 35,
+    home: 36,
+    left: 37,
+    right: 39
+};
+
+const DIRECTION = {
+    [KEYS.left]: -1,
+    [KEYS.right]: 1
+};
+
+$tabs.forEach($tab => {
+    $tab.addEventListener("click", handleClickFromTab);
+    $tab.addEventListener("keyup", handleKeyUpFromTab);
+});
+
+function handleClickFromTab(e) {
+    activateTab(e.target);
+}
+
+function handleKeyUpFromTab(e) {
+    switch(e.keyCode) {
+        case KEYS.left:
+        case KEYS.right:
+            let nextIndex = +e.target.dataset.index + DIRECTION[e.keyCode];
+            if (nextIndex >= $tabs.length) {
+                nextIndex = 0;
+            }
+            if (nextIndex < 0) {
+                nextIndex = ($tabs.length - 1);
+            }
+            if ($tabs[nextIndex]) {
+                activateTab($tabs[nextIndex]);
+            }
+            break;
+        case KEYS.home:
+            activateTab($tabs[0]);
+            break;
+        case KEYS.end:
+            activateTab($tabs[$tabs.length - 1]);
+            break;
+        default:
+            break;
+    }
+}
+
+function activateTab(tab) {
+    deactivateTabs();
+
+    tab.removeAttribute("tabindex");
+    tab.setAttribute("aria-selected", "true");
+    tab.setAttribute("style", "border-top: 1px solid blue;");
+
+    const controls = tab.getAttribute("aria-controls");
+    document.getElementById(controls).removeAttribute("hidden");
+    tab.focus();
+}
+
+function deactivateTabs() {
+    $tabs.forEach(tab => {
+        tab.setAttribute("aria-selected", "false");
+        tab.setAttribute("style", "border-top: 0;");
+        tab.setAttribute("tabindex", "-1");
+    });
+
+    $panels.forEach(panel => {
+        panel.setAttribute("hidden", "");
+    });
+}
+// End: Handle tabs
--- src/templates/subwayStations.js
+++ src/templates/subwayStations.js
@@ -1,3 +1,40 @@
+function renderTabList(data) {
+  return `
+    <div role="tablist">
+      ${data.map((current, index) => {
+        const borough = current.borough;
+
+        return `
+          <button
+            aria-controls="${borough}-panel"
+            aria-selected="${index === 0 ? "true" : "false"}"
+            id="${borough}-tab"
+            role="tab"
+            data-index="${index}"
+          >${borough}</button>
+        `;
+      }).join("")}
+    </div>
+  `;
+}
+
+function renderTabPanels(data) {
+  return data.reduce((accumulated, current, index) => {
+    const borough = current.borough;
+
+    return accumulated += `
+      <ul
+        aria-labelledby="${borough}-tab"
+        id="${borough}-panel"
+        role="tabpanel"
+        ${index === 0 ? "" : "hidden"}
+      >
+        ${stationsTemplate(current.stations, data.subwayLine)}
+      </ul>
+    `;
+  }, "");
+}
+
 function stationsTemplate(stations, subwayLine) {
   return stations.reduce((accumulated, current) => {
     return accumulated += `
@@ -9,27 +46,10 @@ function stationsTemplate(stations, subwayLine) {
 }
 
 function subwayStations(data) {
-  const result = data.reduce((accumulated, current) => {
-    const borough = current.borough;
+  const tabList = renderTabList(data);
+  const tabPanels = renderTabPanels(data);
 
-    return accumulated += `
-    <button
-      aria-controls="${borough}-panel"
-      aria-selected="false"
-      id="${borough}-tab"
-      role="tab"
-    >${borough}</button>
-    <ul
-      aria-labelledby="${borough}-tab"
-      id="${borough}-panel"
-      role="tabpanel"
-    >
-      ${stationsTemplate(current.stations, data.subwayLine)}
-    </ul>
-    `;
-  }, "");
-
-  return result;
+  return `${tabList}${tabPanels}`;
 }
 
 module.exports = subwayStations;
