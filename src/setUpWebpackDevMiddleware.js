c29491939feeeed2e83505c92d811f03b99b14f9
Anthony Ng <newyork.anthonyng@gmail.com>
2019-08-10 12:44:19 -0400

    fix: add babel to fix Safari bugs

--- src/setUpWebpackDevMiddleware.js
+++ src/setUpWebpackDevMiddleware.js
@@ -15,6 +15,23 @@ function webpackDevMiddleware(expressApp) {
             path: path.resolve(__dirname),
             filename: "[name].js",
         },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-env"
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
     });
 
     expressApp.use(
